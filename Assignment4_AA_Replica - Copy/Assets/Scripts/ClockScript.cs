dd04bd52951fa07d9a316d930b08c152c25620eb
Alexander <47425297+ultracloudx@users.noreply.github.com>
2021-03-13 21:18:43 -0600

    2nd commit

---
+++ Assignment4_AA_Replica - Copy/Assets/Scripts/ClockScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    private float timeRemaining = 60f;
+
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    public void Start()
+    {
+        timerIsRunning = true;
+        timeRemaining = PlayerPrefs.GetFloat("MaxTIME");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        DisplayTime(timeRemaining);
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                //Debug.Log(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+        
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
